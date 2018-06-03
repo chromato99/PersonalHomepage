2de73a769933f9e4e7aea73e1b712fe4435d07f9
Beomgi_Kim <chromato99@outlook.com>
2018-06-04 03:02:21 +0900

    split iframe

--- main.js
+++ main.js
@@ -2,21 +2,27 @@ function head_nav(n)
 {
     if(n=='m') {
         document.getElementById('main_frame').src="main.html";
+        document.getElementById('left_nav').src="";
     }
     else if(n=='p') {
-        document.getElementById('main_frame').src="profile_form.html";
+        document.getElementById('main_frame').src="profile_main.html";
+        document.getElementById('left_nav').src="profile_nav.html";
     }
     else if(n=='c') {
-        document.getElementById('main_frame').src="cPro_form.html";
+        document.getElementById('main_frame').src="cPro_main.html";
+        document.getElementById('left_nav').src="cPro_nav.html";
     }
     else if(n=='w') {
-        document.getElementById('main_frame').src="web_form.html";
+        document.getElementById('main_frame').src="web_main.html";
+        document.getElementById('left_nav').src="web_nav.html";
     }
     else if(n=='v') {
-        document.getElementById('main_frame').src="vr_form.html";
+        document.getElementById('main_frame').src="vr_main.html";
+        document.getElementById('left_nav').src="vr_nav.html";
     }
     else if(n=='i') {
-        document.getElementById('main_frame').src="interest_form.html";
+        document.getElementById('main_frame').src="interest_main.html";
+        document.getElementById('left_nav').src="interest_nav.html";
     }
 }
 
@@ -25,7 +31,8 @@ function iframeAutoResize(h) {
         return false;
     }
     
-    h.height = "0px";
-    var iframeHeight = h.contentWindow.document.body.scrollHeight;
-    h.height = iframeHeight;
+    h.style.height = "0px";
+    h.style.height = 'auto';
+    var iframeHeight = h.contentWindow.document.documentElement.scrollHeight + "px";
+    h.style.height = iframeHeight;
 }
