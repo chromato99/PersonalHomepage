9fdc5c383641c5a67ab8858523ad2665429307f5
Beomgi_Kim <chromato99@outlook.com>
2018-06-03 23:05:32 +0900

    before change height function

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function iframeAutoResize(h) {
     if(h==null) {
         return false;
     }
-
+    
     h.height = "0px";
     var iframeHeight = h.contentWindow.document.body.scrollHeight;
     h.height = iframeHeight;
