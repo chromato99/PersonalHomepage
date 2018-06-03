5f7e125e7cbfb4891fee0b732b3acd2ef374a7ba
Beomgi_Kim <chromato99@outlook.com>
2018-06-03 19:48:49 +0900

    change 2018-06-03 19:48

--- main.js
+++ main.js
@@ -4,18 +4,28 @@ function head_nav(n)
         document.getElementById('main_frame').src="main.html";
     }
     else if(n=='p') {
-        document.getElementById('main_frame').src="profile_main.html";
+        document.getElementById('main_frame').src="profile_form.html";
     }
     else if(n=='c') {
-        document.getElementById('main_frame').src="cPro_main.html";
+        document.getElementById('main_frame').src="cPro_form.html";
     }
     else if(n=='w') {
-        document.getElementById('main_frame').src="web_main.html";
+        document.getElementById('main_frame').src="web_form.html";
     }
     else if(n=='v') {
-        document.getElementById('main_frame').src="vr_main.html";
+        document.getElementById('main_frame').src="vr_form.html";
     }
     else if(n=='i') {
-        document.getElementById('main_frame').src="interest_main.html";
+        document.getElementById('main_frame').src="interest_form.html";
     }
+}
+
+function iframeAutoResize(h) {
+    if(h==null) {
+        return false;
+    }
+
+    h.height = "0px";
+    var iframeHeight = h.contentWindow.document.body.scrollHeight;
+    h.height = iframeHeight;
 }
