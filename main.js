688905b98142e083c1de6d8ab216da9d11b1339f
Beomgi_Kim <chromato99@outlook.com>
2018-06-04 13:43:21 +0900

    jQuerry solution

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
 function head_nav(n) 
 {
     if(n=='m') {
-        document.getElementById('main_frame').src="main.html";
-        document.getElementById('left_nav').src="";
+        $("#main_frame").load("main.html");
+        document.getElementById('main_frame').style.backgroundColor="#191919";
+        document.getElementById('main_frame').style.color="#efefef";
+        document.getElementById('left_nav').src="dummy.html";
     }
     else if(n=='p') {
-        document.getElementById('main_frame').src="profile_main.html";
+        $("#main_frame").load("profile_main.html");
+        document.getElementById('main_frame').style.backgroundColor="#efefef";
+        document.getElementById('main_frame').style.color="#191919";
         document.getElementById('left_nav').src="profile_nav.html";
     }
     else if(n=='c') {
-        document.getElementById('main_frame').src="cPro_main.html";
+        $("#main_frame").load("cPro_main.html");
+        document.getElementById('main_frame').style.backgroundColor="#efefef";
+        document.getElementById('main_frame').style.color="#191919";
         document.getElementById('left_nav').src="cPro_nav.html";
     }
     else if(n=='w') {
-        document.getElementById('main_frame').src="web_main.html";
+        $("#main_frame").load("web_main.html");
+        document.getElementById('main_frame').style.backgroundColor="#efefef";
+        document.getElementById('main_frame').style.color="#191919";
         document.getElementById('left_nav').src="web_nav.html";
     }
     else if(n=='v') {
-        document.getElementById('main_frame').src="vr_main.html";
+        $("#main_frame").load("vr_main.html");
+        document.getElementById('main_frame').style.backgroundColor="#efefef";
+        document.getElementById('main_frame').style.color="#191919";
         document.getElementById('left_nav').src="vr_nav.html";
     }
     else if(n=='i') {
-        document.getElementById('main_frame').src="interest_main.html";
+        $("#main_frame").load("interest_main.html");
+        document.getElementById('main_frame').style.backgroundColor="#efefef";
+        document.getElementById('main_frame').style.color="#191919";
         document.getElementById('left_nav').src="interest_nav.html";
     }
-}
-
-function iframeAutoResize(h) {
-    if(h==null) {
-        return false;
-    }
-    
-    h.style.height = "0px";
-    h.style.height = 'auto';
-    var iframeHeight = h.contentWindow.document.documentElement.scrollHeight + "px";
-    h.style.height = iframeHeight;
 }
