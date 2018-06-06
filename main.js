8ee3392e79b6776b1c8e2dba5813b831ebea5894
Beomgi_Kim <chromato99@outlook.com>
2018-06-07 00:39:12 +0900

    final version(still failing the auto resizing

--- main.js
+++ main.js
@@ -2,27 +2,42 @@ function head_nav(n)
 {
     if(n=='m') {
         document.getElementById('main_frame').src="main.html";
-        document.getElementById('main_frame').height="750px";
+        document.getElementById('main_frame').style.height="770px";
         document.getElementById('left_nav').src="";
     }
     else if(n=='p') {
         document.getElementById('main_frame').src="profile_main.html";
         document.getElementById('left_nav').src="profile_nav.html";
+        document.getElementById('main_frame').style.height="770px";
     }
     else if(n=='c') {
         document.getElementById('main_frame').src="cPro_main.html";
         document.getElementById('left_nav').src="cPro_nav.html";
+        document.getElementById('main_frame').style.height="770px";
     }
     else if(n=='w') {
         document.getElementById('main_frame').src="web_main.html";
         document.getElementById('left_nav').src="web_nav.html";
+        document.getElementById('main_frame').style.height="770px";
     }
     else if(n=='v') {
         document.getElementById('main_frame').src="vr_main.html";
         document.getElementById('left_nav').src="vr_nav.html";
+        document.getElementById('main_frame').style.height="770px";
     }
     else if(n=='i') {
         document.getElementById('main_frame').src="interest_main.html";
         document.getElementById('left_nav').src="interest_nav.html";
+        document.getElementById('main_frame').style.height="770px";
     }
+}
+
+function recentResize()
+{
+    document.getElementById('main_frame').style.height="2700px";
+}
+
+function resetHeight()
+{
+    document.getElementById('main_frame').style.height="770px";
 }
